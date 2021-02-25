eae337bbdc10c2d89dfe06dd634293a56973eb22
vamsivallepu <vallepu670@gmail.com>
2021-02-25 23:43:37 +0530

    updated with save option

--- NotePad/src/NotePadGUI.java
+++ NotePad/src/NotePadGUI.java
@@ -25,7 +25,7 @@ import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
 
 public class NotePadGUI implements ActionListener {
-	//System.out.println("Working");
+
 	JFrame frame = new JFrame();
 	JTextArea tArea = new JTextArea(20, 20);
 	Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
